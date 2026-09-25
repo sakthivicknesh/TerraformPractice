ee4261a1c5aaebc56321c9175b307a8402b7f7b2
Sakthi Vicknesh M <sakthivicknesh2003@gmail.com>
2026-09-25 20:48:46 +0530

    First commit for Day2

---
+++ Day_2/terraform.tfvars
@@ -0,0 +1,4 @@
+ami_value = "ami-0b6d9d3d33ba97d99" #Ubuntu Server 26.04 LTS (HVM),EBS General Purpose (SSD) Volume Type
+instance_type_value = "t3.micro" #us-east-1a
+key_name_value = "E-com"
+VM_name = "My EC2 instance"
---
+++ Day_2/variables.tf
@@ -0,0 +1,15 @@
+variable "ami_value" {
+    description = "value for ami"
+}
+
+variable "instance_type_value" {
+    description = "value for instance type"
+}
+
+variable "key_name_value" {
+    description = "value for key_name"
+}
+
+variable "VM_name" {
+    description = "Name of the VM"
+}
